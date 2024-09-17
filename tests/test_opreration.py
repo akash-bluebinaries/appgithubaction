e62571c85d00361ca5a6835e1359a7e0cd25e58d
akash-bluebinaries <akash.dash@bluebinaries.com>
2024-09-17 15:33:10 +0530

    committ

--- tests/test_opreration.py
+++ tests/test_opreration.py
@@ -1,16 +1,11 @@
-from src.math_operations import add, sub
-
+from src.math_operations import add,sub
 
 def test_add():
-    assert add(2,3) == 5
-    assert add(-1,1) == 5
-    assert add(-2,5) == 3
-    assert add(6,0) == 6
-
-
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
 def test_sub():
-    assert sub(5,3) == 2
-    assert sub(1,-1) == 3
-    assert sub(-2,5) == -7
-    assert sub(6,0) == 6
-    assert sub(1000000000,1000000000) == 0
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
